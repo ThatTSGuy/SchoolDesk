ebeeef99dce7c93ed061863465629a8b2291d628
TheOnlyGoodOne <jacktemko2@gmail.com>
2021-11-02 12:36:31 -0400

    Reduced CSS classes

--- utility/widgets.js
+++ utility/widgets.js
@@ -8,14 +8,14 @@ class Sticky {
 			x, y,
 			w: 3, h: 3,
 			content: `
-			<div class="sticky">
+			<div class="widget sticky">
 				<div class="header drag-handle"></div>
-				<div class="editable" contenteditable></div>
+				<div class="content" contenteditable></div>
 			</div>
 			`
 		});
 
-		this.editable = this.cell.querySelector('.editable');
+		this.editable = this.cell.querySelector('.content');
 		this.editable.onclick = this.click.bind(this);
 		this.editable.onfocus = this.focus.bind(this);
 		this.editable.onblur = this.blur.bind(this);
@@ -34,5 +34,22 @@ class Sticky {
 	blur() {
 		this.plainText = this.editable.innerText;
 		this.editable.innerHTML = marked(this.plainText);
+	}
+}
+
+class YouTubeVideo {
+	constructor(url, x, y) {
+		this.cell = desktopGrid.addWidget({
+			x, y,
+			w: 5, h: 3,
+			content: `
+			<div class="widget youtube">
+				<div class="header drag-handle"></div>
+				<iframe class="content" src="https://www.youtube.com/embed/OSbhFr5TzkQ"></iframe>
+			</div>
+			`
+		});
+
+		this.url = url;
 	}
 }
