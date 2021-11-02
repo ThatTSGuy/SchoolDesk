0be57909e38cf6b9465d082cc08f3ea612be3c2d
TheOnlyGoodOne <jacktemko2@gmail.com>
2021-11-02 12:35:31 -0400

    Switched to Contextual.js

--- script.js
+++ script.js
@@ -13,6 +13,40 @@ const desktopGrid = GridStack.init({
 
 new Sticky();
 
+document.oncontextmenu = e => {
+    new Contextual({
+        width: '320px',
+        items: [
+            {
+                type: 'multi', items: [
+                    { label: 'Copy', onClick: () => { console.log('Copy!') } },
+                    { label: 'Cut', onClick: () => { console.log('Cut!') } },
+                    { label: 'Paste', onClick: () => { console.log('Paste!') } },
+                ]
+            },
+            { label: 'Button', onClick: () => { console.log('Item 1 clicked') }, shortcut: 'Ctrl+A' },
+            { type: 'seperator' },
+            {
+                type: 'submenu', label: 'Sub menu', items: [
+                    { label: 'Subitem 1', onClick: () => { } },
+                    { label: 'Subitem 2', onClick: () => { } },
+                    { label: 'Subitem 3', onClick: () => { } },
+                ]
+            },
+            {
+                type: 'hovermenu', label: 'Hover menu', items: [
+                    { label: 'Subitem 1', onClick: () => { } },
+                    { label: 'Subitem 2', onClick: () => { } },
+                    { label: 'Subitem 3', onClick: () => { } },
+                ]
+            },
+            { label: 'Disabled button', onClick: () => { }, shortcut: 'Ctrl+B', enabled: false },
+        ]
+    });
+
+    e.preventDefault();
+}
+
 const menu = new ContextMenu('.grid-stack', [
     {
         name: 'New Sticky',
